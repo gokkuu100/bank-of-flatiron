f3bf4e79814360bf565f57add66a0f537a841639
goku <wkurts247@gmail.com>
2023-07-23 10:02:27 +0300

    Created table

--- bank-of-flatiron/src/TransactionList.js
+++ bank-of-flatiron/src/TransactionList.js
@@ -3,6 +3,23 @@ import React, {useState} from "react";
 const TransactionList = () => {
     const [transactions, setTransactions] = useState([])
 
+    return (
+        <>
+        <h3>Transaction table</h3>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Date</th>
+                <th>Description</th>
+                <th>Category</th>
+                <th>Amount</th>
+            </tr>
+            <tr>
+                
+            </tr>
+        </table>
+        </>
+    )
 
 }
 export default TransactionList
