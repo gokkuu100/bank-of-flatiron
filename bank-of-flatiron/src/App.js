837ce9b8ba314eb44cdab29dcdc021089da797d5
goku <wkurts247@gmail.com>
2023-07-23 14:12:34 +0300

    Transaction listing complete

--- bank-of-flatiron/src/App.js
+++ bank-of-flatiron/src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
-//import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import TransactionList from './TransactionList';
+import TransactionForm from './TransactionForm';
+import SearchList from './SearchList';
 
 function App() {
+
+  const [transactions, setTransactions] = useState([]);
+
+  const addTransaction = (formData) => {
+    setTransactions((prevTransactions) => [...prevTransactions, formData]);
+  };
+  
   return (
   <>
   <h1>Bank of FlatIron</h1>
-  <TransactionList />
+  <TransactionList transactions={transactions} />
+  <SearchList transactions={transactions} />
+  <TransactionForm addTransaction={addTransaction}/>
+  
   </>
   )
 }
--- bank-of-flatiron/src/TransactionList.js
+++ bank-of-flatiron/src/TransactionList.js
@@ -1,37 +1,75 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from 'react';
+//import TransactionForm from './TransactionForm';
 
 const TransactionList = () => {
-    const [transactions, setTransactions] = useState([])
+  const [transactions, setTransactions] = useState({}); // Change initial state to an object
 
-    useEffect(() => {
-        
-    })
+  useEffect(() => {
+    fetch('http://localhost:3001/transactions')
+      .then((resp) => {
+        if (!resp.ok) {
+          throw new Error('Error fetching data');
+        }
+        return resp.json();
+      })
+      .then((data) => {
+        //console.log('Received data:', data);
+        if (!data || typeof data !== 'object' || Object.keys(data).length === 0) {
+          throw new Error('Wrong format');
+        }
+        setTransactions(data); // Save the fetched data as an object
+      })
+      .catch((error) => {
+        console.error('Error when fetching data:', error);
+      });
+  }, []);
 
-    return (
-        <>
-        <h3>Transaction table</h3>
-        <table>
+//   const addTransaction = (newTransaction) => {
+//     // Generate a new unique ID (you can use a library like 'uuid' for this purpose)
+//     const newId = Object.keys(transactions).length + 1;
+
+//     // Create the new transaction object with the new ID
+//     const transactionToAdd = { ...newTransaction, id: newId };
+
+//     // Update the transactions state with the new transaction added
+//     setTransactions((prevTransactions) => ({
+//       ...prevTransactions,
+//       [newId]: transactionToAdd,
+//     }));
+//   };
+
+  if (!transactions || typeof transactions !== 'object' || Object.keys(transactions).length === 0) {
+    return null;
+  }
+
+  return (
+    <>
+      
+      <h3>Transaction table</h3>
+      <table>
+        <thead>
             <tr>
-                <th>ID</th>
-                <th>Date</th>
-                <th>Description</th>
-                <th>Category</th>
-                <th>Amount</th>
+            <th>ID</th>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.values(transactions).map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.id}</td>
+              <td>{transaction.date}</td>
+              <td>{transaction.description}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.amount}</td>
             </tr>
-            <tr>
-                {transactions.map(transaction => (
-                    <tr key={transaction.id}>
-                        <td>{transaction.id}</td>
-                        <td>{transaction.date}</td>
-                        <td>{transaction.description}</td>
-                        <td>{transaction.category}</td>
-                        <td>{transaction.amount}</td>
-                    </tr>
-                ))}
-            </tr>
-        </table>
-        </>
-    )
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
 
-}
-export default TransactionList
+export default TransactionList;
