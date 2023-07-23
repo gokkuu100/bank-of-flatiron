2d9072de4e47957811ae35b8baa1cf29bb36497d
goku <wkurts247@gmail.com>
2023-07-23 14:34:04 +0300

    Searchlist form

--- bank-of-flatiron/src/SearchList.js
+++ bank-of-flatiron/src/SearchList.js
@@ -1,3 +1,55 @@
 import React, { useState, useEffect } from "react";
 
+const SearchList = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [transactions, setTransactions] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    return (
+        
+        <>
+          <h2>Search Transaction</h2>
+          <form>
+            <label>Search: </label>
+            <input
+              type="text"
+              value={searchTerm}
+              onChange={handleSearch}
+              placeholder="Search by description..."
+            />
+          </form>
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <>
+              <p>Transactions done:</p>
+              <table>
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Date</th>
+                    <th>Description</th>
+                    <th>Category</th>
+                    <th>Amount</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {filteredTransactions.map((transaction) => (
+                    <tr key={transaction.id}>
+                      <td>{transaction.id}</td>
+                      <td>{transaction.date}</td>
+                      <td>{transaction.description}</td>
+                      <td>{transaction.category}</td>
+                      <td>{transaction.amount}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </>
+          )}
+        </>
+      );
+
+}
+
 export default SearchList;
