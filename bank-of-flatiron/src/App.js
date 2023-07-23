cb0f7f06998615453788d9ef0e225007cdf881c8
goku <wkurts247@gmail.com>
2023-07-23 09:40:24 +0300

    Created react app

---
+++ bank-of-flatiron/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <>
+  <h1>Bank of FlatIron</h1>
+  </>
+  )
+}
+
+
+export default App;
